c57eaeee61008dea82d81c025133eb7c44f19527
polushindn <polushin@sdl-arsenal.ru>
2026-08-19 11:48:16 +0700

    CorsConfig

--- src/main/kotlin/com/example/oracleapi/config/CorsConfig.kt
+++ src/main/kotlin/com/example/oracleapi/config/CorsConfig.kt
@@ -5,17 +5,17 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
-
 @Configuration
 class CorsConfig : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:5173")
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedOriginPatterns("*") // Разрешаем любые домены и порты
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
             .allowedHeaders("*")
             .allowCredentials(true)
             .maxAge(3600)
     }
+
     override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
         // Маппинг /markdown/** на папку classpath:/markdown/
         registry.addResourceHandler("/markdown/**")
